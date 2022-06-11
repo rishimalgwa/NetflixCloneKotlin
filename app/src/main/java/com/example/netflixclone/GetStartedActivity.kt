468d79520b05f78f009091e03660feed1f4d567e
Rishi Malgwa <rishimalgwa35@gmail.com>
2022-06-11 17:09:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/netflixclone/GetStartedActivity.kt
@@ -0,0 +1,14 @@
+package com.example.netflixclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class GetStartedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_started)
+        supportActionBar?.hide()
+
+    }
+}
